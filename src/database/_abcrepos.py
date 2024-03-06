116e538c56d20ba167ccfcb64f6cb63d5f4763f8
Mark <vt24062406@gmail.com>
2024-03-06 16:01:26 +0300

    Create _abcrepos.py

---
+++ src/database/_abcrepos.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class Repository(ABC):
+    model: DeclarativeBase
+    session: AsyncSession
+
+    def __init__(self, session: AsyncSession) -> None:
+        self.session: AsyncSession = session
+
+    @abstractmethod
+    def create():
+        ...
+
+    @abstractmethod
+    def update():
+        ...
+
+    @abstractmethod
+    def delete():
+        ...
+
+    @abstractmethod
+    def get():
+        ...
+
+        
